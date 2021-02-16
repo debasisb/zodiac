78214337c1487c1cf46c09ebb1f7fb52adaa510e
mtexe <70678819+mtexe@users.noreply.github.com>
2021-02-16 11:25:39 -1000

    Updated index.js
    Changed the dates.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const zodiacSign = require('get-zodiac-sign');
-var myZodiacSign1 = zodiacSign(3,13);
-var myZodiacSign2 = zodiacSign(2,18);
-var myZodiacSign3 = zodiacSign(4,27);
+var myZodiacSign1 = zodiacSign(10,26);
+var myZodiacSign2 = zodiacSign(5,18);
+var myZodiacSign3 = zodiacSign(6,13);
 console.log(myZodiacSign1);
 console.log(myZodiacSign2);
 console.log(myZodiacSign3);
